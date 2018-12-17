eecdb12dc1fa80a300904e622028e378eb120c3f
ramsel <ramsel.batara@gmail.com>
2018-12-18 03:18:35 +0700

    second Commit

--- Exercise14.js
+++ Exercise14.js
@@ -1,31 +1,22 @@
 function changeVocals(str) {
-  var result = "";
-  for (var i = 0; i < str.length; i++) {
-    if (str[i] === "a") {
-      result += "b";
-    } else if (str[i] === "i") {
-      result += "j";
-    } else if (str[i] === "u") {
-      result += "v";
-    } else if (str[i] === "e") {
-      result += "f";
-    } else if (str[i] === "o") {
-      result += "p";
-    } else if (str[i] === "A") {
-      result += "B";
-    } else if (str[i] === "I") {
-      result += "J";
-    } else if (str[i] === "U") {
-      result += "V";
-    } else if (str[i] === "E") {
-      result += "F";
-    } else if (str[i] === "O") {
-      result += "P";
-    } else {
-      result += str[i];
-    }
+  var result = ""
+  var vocal = 'aiueoAIUEO'
+  var newVocal = 'bjvfpBJVFP'
+  
+  var flag = false
+  for(var i = 0; i < str.length; i++){
+      for(var j = 0; j < vocal.length; j++){
+          if(str[i] === vocal[j]){
+              result += newVocal[j]
+              flag = true
+          }
+      }
+      if(flag === false){
+          result += str[i]
+      }
+      flag = false
   }
-  return result;
+return result
 }
 
 function reverseWord(str) {
